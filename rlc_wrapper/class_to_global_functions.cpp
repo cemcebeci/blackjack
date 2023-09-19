1b60a253bf7d8e9bccd246921ece3c51cb94a561
Cem Cebeci <cemcebeci99@gmail.com>
2023-09-19 17:05:08 +0200

    confused.

--- rlc_wrapper/class_to_global_functions.cpp
+++ rlc_wrapper/class_to_global_functions.cpp
@@ -1,29 +1,46 @@
 #include "../src/blackjack.cpp"
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
 
-extern "C" Blackjack *Blackjack_newint64_t_int64_t_(int *num_players) {
+extern "C" Blackjack *Blackjack_newint64_t_int64_t_(void* _, uint64_t *num_players) {
+
+    //?? Why does this not work?
+    // auto ret = new Blackjack(*num_players);
+    // std::cout << "ret" << " -> " << ret << std::endl;
+    // *retval = ret; 
+    // return ret
+
+    //?? But this one does
     return new Blackjack(*num_players);
 }
 
-extern "C" void Blackjack_shufflevoid_int64_t_(Blackjack **blackjack) {
-    (*blackjack)->shuffle_deck();
+extern "C" void Blackjack_shufflevoid_int64_t_(void* _, Blackjack *blackjack) {
+    //? Why does RLC pass a Blackjack* here and not a Blackjack**?
+    std::cout << blackjack << std::endl;
+    (blackjack)->shuffle_deck();
+    std::cout << "SHUFFLE DONE" << std::endl;
 }
 
-extern "C" void Blackjack_dealvoid_int64_t_(Blackjack **blackjack) {
-    (*blackjack)->deal();
+extern "C" void Blackjack_dealvoid_int64_t_(void* _, Blackjack *blackjack) {
+    std::cout << blackjack << std::endl;
+    (blackjack)->deal();
+    std::cout << "DEAL DONE" << std::endl;
 }
 
-extern "C" void Blackjack_drawvoid_int64_t_int64_t_(Blackjack **blackjack, int *player) {
-    (*blackjack)->take_card(*player);
+extern "C" void Blackjack_drawvoid_int64_t_int64_t_(void* _, Blackjack *blackjack, int *player) {
+    (blackjack)->take_card(*player);
+    std::cout << "TAKE DONE" << std::endl;
 }
 
-extern "C" void Blackjack_passvoid_int64_t_int64_t_(Blackjack **blackjack, int *player) {
-    (*blackjack)->pass(*player);
+extern "C" void Blackjack_passvoid_int64_t_int64_t_(void* _, Blackjack *blackjack, int *player) {
+    (blackjack)->pass(*player);
 }
 
-extern "C" void Blackjack_dump_statevoid_int64_t_(Blackjack **blackjack) {
-    (* blackjack)->dump_state();
+extern "C" void Blackjack_dump_statevoid_int64_t_(void* _, Blackjack *blackjack) {
+    (blackjack)->dump_state();
 }
 
-extern "C" int Blackjack_calculate_winnerint64_t_int64_t_(Blackjack **blackjack) {
-    return (* blackjack)->calculate_winner();
+extern "C" int Blackjack_calculate_winnerint64_t_int64_t_(void* _, Blackjack *blackjack) {
+    return (blackjack)->calculate_winner();
 }
